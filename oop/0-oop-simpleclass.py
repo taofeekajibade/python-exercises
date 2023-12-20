afbf2565b20ca1625b14d459528b5f9a6f12c6be
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-12-20 09:21:03 +0100

    edited codes and filenames

--- oop/0-oop-simpleclass.py
+++
@@ -1,7 +0,0 @@
-# create a new class with the keyword class and the class name Person.
-class Person:
-    pass                    # An empty block indicated by pass statement.
-
-# create an object of the class with the class name.
-p = Person() 
-print(p)
